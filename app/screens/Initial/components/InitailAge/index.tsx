e81d9c387e8ea07870099e71967d9088a31d6aba
Sera.Hu <hulixiaSera@gmail.com>
2025-10-16 10:38:25 +0800

    fix: style

--- app/screens/Initial/components/InitailAge/index.tsx
+++ app/screens/Initial/components/InitailAge/index.tsx
@@ -1,21 +1,28 @@
-import { memo } from "react";
-import FlatSelectorLikeIOS from "../../../../styles/FlatSelectorLikeIOS";
-import { AgeContainer } from "./style";
+import { memo } from 'react';
+import FlatSelectorLikeIOS from '../../../../styles/FlatSelectorLikeIOS';
+import { AgeContainer } from './style';
+
+const INIT_AGE = 6;
+const FINAL_AGE = 99;
+const AGE_GAP = FINAL_AGE - INIT_AGE + 1;
 
 const InitailAge = () => {
-
-  const ageArray = Array.from({ length: 94 }, (_, index) => index + 6).map((item) => {
-    return {
-      key: item.toString(),
-      title: item.toString(),
-    }
-  });
+  const ageArray = Array.from(
+    { length: AGE_GAP },
+    (_, index) => index + INIT_AGE,
+  ).map(item => ({
+    key: item.toString(),
+    title: item.toString(),
+  }));
 
   return (
     <AgeContainer>
-      <FlatSelectorLikeIOS data={ageArray} defaultValue='6' />
+      <FlatSelectorLikeIOS 
+        data={ageArray} 
+        defaultValue={INIT_AGE.toString()}
+      />
     </AgeContainer>
-  )
-}
+  );
+};
 
-export default memo(InitailAge)
+export default memo(InitailAge);
