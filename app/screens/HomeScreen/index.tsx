644e5a2d12c5903f2394711a5b7024073989ca67
Sera.Hu <hulixiaSera@gmail.com>
2025-10-20 15:36:25 +0800

    ci: add zustand

--- app/screens/HomeScreen/index.tsx
+++ app/screens/HomeScreen/index.tsx
@@ -1,11 +1,30 @@
 import { memo } from 'react';
-import { Text } from 'react-native-paper';
+import { Button, Text } from 'react-native-paper';
 import { useTranslation } from 'react-i18next';
+import { useSportHistoryStore } from '@/stores';
 
 const HomeScreen = () => {
+  const sportHistory = useSportHistoryStore(state => state.sportHistory)
+  const addSportRecord = useSportHistoryStore(state => state.addSportRecord)
   const { t } = useTranslation();
+  return (
+    <>
+      <Text>{t('home.title')}</Text>
+      {
+        sportHistory.map(item => {
+          return <Text>{item.sportCategory.toString()}</Text>
+        })
+      }
 
-  return <Text>{t('home.title')}</Text>;
+      <Button onPress={() => {
+        addSportRecord({
+          id: 'w',
+          duration: 8000,
+          sportCategory: ['游泳']
+        })
+      }}>添加</Button>
+    </>
+  );
 };
 
 export default memo(HomeScreen);
---
+++ app/screens/ProfileScreen/style.ts
@@ -0,0 +1,15 @@
+import { Button, Text } from "react-native-paper";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+`
+
+export const ContainerTitle = styled(Text)`
+  margin-bottom: 24px;
+`
+
+export const ChangeBtn = styled(Button)`
+  margin-top: 16px;
+`
