120d3dfcfa41e2c321491a2454dff894cb759c81
Sera.Hu <hulixiaSera@gmail.com>
2025-10-21 23:19:23 +0800

    feat: card syle

--- app/screens/HomeScreen/components/OverviewCard/index.tsx
+++ app/screens/HomeScreen/components/OverviewCard/index.tsx
@@ -1,7 +1,8 @@
 import React, { memo } from "react";
 import { useTranslation } from 'react-i18next';
-import { OverviewBottomContainer, OverviewContainer, OverviewTopContainer, OverviewTopLeftContainer, OverviewTopRightContainer, Row } from "./style";
-import { Divider, Icon, Text, useTheme } from "react-native-paper";
+import { OverviewBottomContainer, OverviewContainer, OverviewTopContainer, OverviewTopLeftContainer, OverviewTopRightContainer, Row, TargetValueText, OverviewBottomTitle } from "./style";
+import { Icon, Text, useTheme } from "react-native-paper";
+import OverviewSection from "./OverviewSection";
 
 const OverviewCard = () => {
   const theme = useTheme()
@@ -10,54 +11,42 @@ const OverviewCard = () => {
   return (
     <OverviewContainer>
       <OverviewTopContainer>
-        {/* 左侧 - 纵向排列 */}
         <OverviewTopLeftContainer>
           <Row justifyContent='flex-start'>
-            <Icon source='cup-water' size={24} color={theme.colors.primary} />
-            <Text>{t('home.sportTime')}</Text>
-            <Icon source='chevron-right' size={24} color={theme.colors.primary} />
+            <Icon source='run' size={24} color={theme.colors.tertiary} />
+            <Text variant='titleSmall'>{t('home.sportTime')}</Text>
+            <Icon source='chevron-right' size={20} color={theme.colors.onSurfaceVariant} />
           </Row>
-          <Text>30</Text>
-          <Text>{t('home.sportTimeTarget', { time: 60 })}</Text>
+          <Text variant='displayLarge'>30</Text>
+          <TargetValueText>{t('home.sportTimeTarget', { time: 60 })}</TargetValueText>
         </OverviewTopLeftContainer>
 
-        {/* 右侧 - 纵向排列，内部有横向排列 */}
         <OverviewTopRightContainer>
-          {/* 步数部分 */}
-          <Row justifyContent='flex-start'>
-            <Icon source='cup-water' size={24} color={theme.colors.primary} />
-            <Text>{t('home.steps')}</Text>
-            <Icon source='chevron-right' size={24} color={theme.colors.primary} />
-          </Row>
-          <Row>
-            <Text>30</Text>
-            <Text>{t('home.stepsTarget', { time: 3000 })}</Text>
-          </Row>
-          
-          <Divider />
-          
-          {/* 卡路里部分 */}
-          <Row justifyContent='flex-start'>
-            <Icon source='cup-water' size={24} color={theme.colors.primary} />
-            <Text>{t('home.calories')}</Text>
-            <Icon source='chevron-right' size={24} color={theme.colors.primary} />
-          </Row>
-          <Row>
-            <Text>30</Text>
-            <Text>{t('home.caloriesTarget', { time: 60 })}</Text>
-          </Row>
+          <OverviewSection
+            icon="walk"
+            iconColor={theme.colors.primary}
+            title={t('home.steps')}
+            currentValue={30}
+            targetText={t('home.stepsTarget', { time: 3000 })}
+          />
+
+          <OverviewSection
+            icon="fire"
+            iconColor={theme.colors.error}
+            title={t('home.calories')}
+            currentValue={30}
+            targetText={t('home.caloriesTarget', { time: 60 })}
+            withBorder
+          />
         </OverviewTopRightContainer>
       </OverviewTopContainer>
-      
-      <Divider />
-      
-      {/* 底部 - 保持原样 */}
+
       <OverviewBottomContainer>
-        <Row>
-          <Icon source='cup-water' size={24} color={theme.colors.primary} />
+        <OverviewBottomTitle>
+          <Icon source='cup-water' size={24} color={theme.colors.secondary} />
           <Text>{t('home.drink')}{t('home.drinkCurrent', { time: 6 })}</Text>
-        </Row>
-        <Icon source='chevron-right' size={24} color={theme.colors.primary} />
+        </OverviewBottomTitle>
+        <Icon source='chevron-right' size={20} color={theme.colors.onSurfaceVariant} />
       </OverviewBottomContainer>
     </OverviewContainer>
   )
